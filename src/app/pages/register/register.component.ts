2c199295020ecd9b131264f2af1ebb3e1b3a21e8
Akash Waghmare <akashwaghmareangs@gmail.com>
2025-07-09 17:20:49 +0530

    added_Register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  registerObj: any = {
+    "fullName": "",
+    "email": "",
+    "password": "",
+    "collegeName": "",
+    "role": ""
+  }
+
+  http = inject(HttpClient)
 
 }
